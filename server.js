2f6507b6fb9f9780f113f3d2c3d329a590d4aaa6
adabo4 <130857407+adabo4@users.noreply.github.com>
2023-10-21 21:17:38 +0200

    toggle check update

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.use(express.json())
 connectToDatabase();
 
 // Serve the React app's static files from the build directory
-//app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 //Use the app in development mode
-app.get('/', (req, res) => {
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
+// app.get('/', (req, res) => {
+//     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+// });
 
 app.use("/patients", require('./routes/api'));
 app.use("/login", require('./routes/post'))
